07b648c5e503baa72f7ecd493e6e9396d2ed8a59
Pedro Falco <fpfalco@gmail.com>
2024-10-03 11:45:09 -0300

    feat: add reactive available mints and congratulations

--- src/components/frontend/MainExecutoor.svelte
+++ src/components/frontend/MainExecutoor.svelte
@@ -1,6 +1,18 @@
-<script>
+<script lang="ts">
+    import { PUBLIC_TOTAL_SUPPLY } from '$env/static/public';
+    import { getAvailableMints } from '$lib/props-mint';
+    import MintTransactionStore from '$store/MintTransactionStore';
+	import { onMount } from 'svelte';
+
     let minting = true;
-    $: minted = 1200;
+	let availableAssets: number;
+	let totalSupply: number = Number(PUBLIC_TOTAL_SUPPLY);
+    
+	onMount(async () => {
+		availableAssets = await getAvailableMints();
+		console.log('Available assets on mount:', availableAssets);
+	});
+
 </script>
 <div class="font-schmaltzy fixed bottom-0 left-0 lg:left-1/4 lg:-translate-x-1/2 flex items z-10">
     <img draggable="false" class="select-none lg:hidden h-full" src="/media/executoor-mobile.png" alt="">
@@ -9,12 +21,20 @@
     <div class="absolute bottom-2 lg:bottom-auto lg:top-0 left-1/2 lg:left-2/3 w-full max-w-sm">
         <img draggable="false" class="select-none relative scale-y-[-1] lg:scale-y-100" src="/media/speech-bubble.png" alt="">
 
-        <div class="absolute right-0 top-1/2 -translate-y-1/2 text-center lg:space-y-2">
+        <div class="absolute right-1/3 translate-x-1/3 top-1/2 -translate-y-1/2 text-center lg:space-y-2 flex flex-col items-center justify-center">
             {#if minting}
-            <p class="text-2xl lg:text-5xl text-[#0290d9] text-stroke-black mr-8 lg:mr-10">{ `${minted} / 6942`}</p>
-            <p class="text-xl lg:text-3xl text-[#0290d9] text-stroke-black uppercase mr-5 lg:mr-7">minted</p>
+                {#if availableAssets != undefined}
+                    {#if $MintTransactionStore.status != "confirmed"}
+                        <p class="text-2xl lg:text-5xl text-[#0290d9] text-stroke-black">{ `${totalSupply - availableAssets} / ${totalSupply}`}</p>
+                        <p class="text-xl lg:text-3xl text-[#0290d9] text-stroke-black uppercase">minted</p>
+                    {:else}
+                        <p class="text-lg lg:text-3xl text-[#0290d9] text-stroke-black">congratulations!</p>
+                    {/if}
+                {:else}
+                    <p class="text-xl lg:text-3xl text-[#0290d9] text-stroke-black">finding executoors</p>
+                {/if}
             {:else}
-            <p class="lg:text-3xl text-[#0290d9] text-stroke-black uppercase mr-5 lg:mr-7">congratulations!</p>
+            <p class="lg:text-3xl text-[#0290d9] text-stroke-black uppercase mr-5 lg:mr-7">Minted Out</p>
             {/if}
         </div>
     </div>
