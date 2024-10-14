b6fe065bc711880ff8bbb75a37bfe8e60ec38932
Pedro Falco <fpfalco@gmail.com>
2024-10-13 23:54:09 -0300

    chore: add convertWallet function

--- src/components/wallet/WalletComponent.svelte
+++ src/components/wallet/WalletComponent.svelte
@@ -3,6 +3,7 @@
 	import WalletButtonStore from '$store/WalletButtonsStore';
 	import walletStore from '$store/walletStore';
 	import Modal from './WalletModal.svelte';
+	import { Address } from 'fuels';
 
 	let showModal: boolean = false;
 
@@ -10,10 +11,17 @@
 		if (!address) {
 			return '';
 		}
+        address = convertWallet(address);
+        console.log(address)
 		return `${address.slice(0, 6)}...${address.slice(-4)}`;
 	};
+    
+	const convertWallet = (address: string) => {
+		return address.toLowerCase().includes('fuel')
+			? Address.fromAddressOrString(address).toB256()
+			: address;
+	};
 </script>
-
 <div 
 	class="absolute right-2 md:right-10 top-0 text-center bg-no-repeat bg-center bg-contain w-32 md:w-52 h-12 md:h-20 z-10" 
     style="background-image: url('/media/wallet-bg.png')">
